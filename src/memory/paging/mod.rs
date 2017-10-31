bb3a30180e110cd2b58f802611f1347a18ed7564
Uzzell, Harry <harry.uzzell@roke.co.uk>
2017-10-31 15:47:55 +0000

    Completed blog post 6. Page tables and entries are now modelled. Still need a `deallocate_frame` method (... impl in post 7 ??). The TLB must be manually updated when a page mapping is changed.

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -1,7 +1,13 @@
-use memory::{PAGE_SIZE, Frame, FrameAllocator};
 pub use self::entry::*;
 
+use memory::{PAGE_SIZE, Frame, FrameAllocator};
+use self::table::{Table, Level4};
+
+use core::ptr::Unique;
+use x86_64::instructions::tlb;
+
 mod entry;
+mod table;
 
 const ENTRY_COUNT: usize = 512;
 
@@ -10,11 +16,6 @@ pub type VirtualAddress  = usize;
 
 pub struct Page {
     number: usize,
-}
-
-pub fn translate(virtual_address: VirtualAddress) -> Option<PhysicalAddress> {
-    let offset = virtual_address % PAGE_SIZE;
-    translate_page(Page::containing_address(virtual_address)).map(|frame| frame.number * PAGE_SIZE + offset)
 }
 
 impl Page {
@@ -45,56 +46,112 @@ impl Page {
     }
 }
 
-fn translate_page(page: Page) -> Option<Frame> {
-    use self::entry::HUGE_PAGE;
-
-    let p3 = unsafe { &*table::P4 }.next_table(page.p4_index());
-
-    let huge_page = || {
-        p3.and_then(|p3| {
-            let p3_entry = &p3[page.p3_index()];
-            if let Some(start_frame) = p3_entry.pointed_frame() {
-                if p3_entry.flags().contains(HUGE_PAGE) {
-                    assert!(start_frame.number % (ENTRY_COUNT * ENTRY_COUNT) == 0);
-                    return Some(Frame {
-                        number: start_frame.number + page.p2_index() *
-                            ENTRY_COUNT + page.p1_index(),
-                    });
-                }
-            }
-            if let Some(p2) = p3.next_table(page.p3_index()) {
-                let p2_entry = &p2[page.p2_index()];
-                if let Some(start_frame) = p2_entry.pointed_frame() {
-                    if p2_entry.flags().contains(HUGE_PAGE) {
-                        assert!(start_frame.number % ENTRY_COUNT == 0);
-                        return Some(Frame {
-                            number: start_frame.number + page.p1_index()
-                        });
-                    }
-                }
-            }
-            None
-        })
-    };
-
-    p3.and_then(|p3| p3.next_table(page.p3_index()))
-        .and_then(|p2| p2.next_table(page.p2_index()))
-        .and_then(|p1| p1.next_table(page.p1_index()))
-        .or_else(huge_page)
+pub struct ActivePageTable {
+    p4: Unique<Table<Level4>>,
 }
 
-pub use self::entry::*;
-use memory::FrameAllocator;
+impl ActivePageTable {
+    pub unsafe fn new() -> ActivePageTable {
+        ActivePageTable {
+            p4: Unique::new_unchecked(table::P4),
+        }
+    }
 
-pub fn map_to<A>(page: Page, frame: Frame, flags: EntryFlags,
-                 allocator: &mut A)
-    where A: FrameAllocator
-{
-    let p4 = unsafe { &mut *P4 };
-    let mut p3 = p4.next_table_create(page.p4_index(), allocator);
-    let mut p2 = p3.next_table_create(page.p3_index(), allocator);
-    let mut p1 = p2.next_table_create(page.p2_index(), allocator);
+    fn p4(&self) -> &Table<Level4> {
+        unsafe { self.p4.as_ref() }
+    }
 
-    assert!(p1[page.p1_index()].is_unused());
-    p1[page.p1_index()].set(frame, flags | PRESENT);
+    fn p4_mut(&mut self) -> &mut Table<Level4> {
+        unsafe { self.p4.as_mut() }
+    }
+
+    pub fn translate(&self, virtual_address: VirtualAddress) -> Option<PhysicalAddress> {
+        let offset = virtual_address % PAGE_SIZE;
+        self.translate_page(Page::containing_address(virtual_address)).map(|frame| frame.number * PAGE_SIZE + offset)
+    }
+
+    pub fn translate_page(&self, page: Page) -> Option<Frame> {
+        use self::entry::HUGE_PAGE;
+
+        let p3 = self.p4().next_table(page.p4_index());
+
+        let huge_page = || { // This is called if any of the steps through tables 
+            // on the way to a valid frame return None - i.e. the 
+            // next table has an entry that is either not present
+            // or a huge page. If it is a huge page, this closure
+            // correctly translates it.
+            p3.and_then(|p3| {
+                let p3_entry = &p3[page.p3_index()];
+                if let Some(start_frame) = p3_entry.pointed_frame() {
+                    if p3_entry.flags().contains(HUGE_PAGE) {
+                        assert!(start_frame.number % (ENTRY_COUNT * ENTRY_COUNT) == 0);
+                        return Some(Frame { number: start_frame.number + page.p2_index() * ENTRY_COUNT + page.p1_index(), });
+                    }
+                }
+                if let Some(p2) = p3.next_table(page.p3_index()) {
+                    let p2_entry = &p2[page.p2_index()];
+                    if let Some(start_frame) = p2_entry.pointed_frame() {
+                        if p2_entry.flags().contains(HUGE_PAGE) {
+                            assert!(start_frame.number % ENTRY_COUNT == 0);
+                            return Some(Frame { number: start_frame.number + page.p1_index() });
+                        }
+                    }
+                }
+                None
+            })
+        };
+
+        p3.and_then(|p3| p3.next_table(page.p3_index()))
+            .and_then(|p2| p2.next_table(page.p2_index()))
+            .and_then(|p1| p1[page.p1_index()].pointed_frame())
+            .or_else(huge_page)
+    }
+
+    pub fn map_to<A>(&mut self,
+                     page: Page,
+                     frame: Frame,
+                     flags: EntryFlags,
+                     allocator: &mut A)
+        where A: FrameAllocator
+        {
+            let mut p3 = self.p4_mut().next_table_create(page.p4_index(), allocator);
+            let mut p2 = p3.next_table_create(page.p3_index(), allocator);
+            let mut p1 = p2.next_table_create(page.p2_index(), allocator);
+
+            assert!(p1[page.p1_index()].is_unused());
+            p1[page.p1_index()].set(frame, flags | PRESENT);
+        }
+
+    pub fn map<A>(&mut self, page: Page, flags: EntryFlags, allocator: &mut A)
+        where A: FrameAllocator
+        {
+            let frame = allocator.allocate_frame().expect("out of memory");
+            self.map_to(page, frame, flags, allocator)
+        }
+
+    pub fn identity_map<A>(&mut self, frame: Frame, flags: EntryFlags, allocator: &mut A)
+        where A: FrameAllocator
+        {
+            let page = Page::containing_address(frame.start_address());
+            self.map_to(page, frame, flags, allocator)
+        }
+
+    fn unmap<A>(&mut self, page: Page, allocator: &mut A)
+        where A: FrameAllocator
+        {
+            assert!(self.translate(page.start_address()).is_some());
+
+            let p1 = self.p4_mut()
+                .next_table_mut(page.p4_index())
+                .and_then(|p3| p3.next_table_mut(page.p3_index()))
+                .and_then(|p2| p2.next_table_mut(page.p2_index()))
+                .expect("Mapping huge pages is unsupported in huOS");
+            let frame = p1[page.p1_index()].pointed_frame().unwrap();
+            p1[page.p1_index()].set_unused();
+
+            use x86_64::VirtualAddress;
+            tlb::flush(VirtualAddress(page.start_address()));
+            // TODO free p(1, 2, 3) tables if empty.
+            allocator.deallocate_frame(frame);
+        }
 }
--- src/memory/paging/table.rs
+++ src/memory/paging/table.rs
@@ -1,6 +1,7 @@
 use core::ops::{Index, IndexMut};
 use core::marker::PhantomData;
 
+use memory::FrameAllocator;
 use memory::paging::entry::*;
 use memory::paging::ENTRY_COUNT;
 
@@ -11,7 +12,7 @@ pub struct Table<L: TableLevel> {
     level: PhantomData<L>,
 }
 
-impl<L> Index<usize> for table<L> where L: TableLevel
+impl<L> Index<usize> for Table<L> where L: TableLevel
 {
     type Output = Entry;
 
@@ -20,7 +21,7 @@ impl<L> Index<usize> for table<L> where L: TableLevel
     }
 }
 
-impl<L> IndexMut<usize> for table<L> where L: TableLevel
+impl<L> IndexMut<usize> for Table<L> where L: TableLevel
 {
     fn index_mut(&mut self, index: usize) -> &mut Entry {
         &mut self.entries[index]
@@ -38,23 +39,36 @@ impl<L> Table<L> where L: TableLevel
 
 impl<L> Table<L> where L: HierarchicalLevel
 {
-    fn next_table(&self, index: usize) -> Option<&Table<L::NextLevel>> {
+    pub fn next_table(&self, index: usize) -> Option<&Table<L::NextLevel>> {
         self.next_table_address(index).map(|address| unsafe { &*(address as *const _) })
     }
 
-    fn next_table_mut(&mut self, index: usize) -> Option<&mut Table<L::NextLevel>> {
+    pub fn next_table_mut(&mut self, index: usize) -> Option<&mut Table<L::NextLevel>> {
         self.next_table_address(index).map(|address| unsafe { &mut *(address as *mut _) })
     }
 
     fn next_table_address(&self, index: usize) -> Option<usize> {
         let entry_flags = self[index].flags();
         if entry_flags.contains(PRESENT) && !entry_flags.contains(HUGE_PAGE) {
-            let table_address = self as *const_ usize;
+            let table_address = self as *const _ as usize;
             Some((table_address << 9) | (index << 12))
         } else {
             None
         }
     }
+
+    pub fn next_table_create<A>(&mut self, index: usize, allocator: &mut A) -> &mut Table<L::NextLevel>
+        where A: FrameAllocator
+        {
+            if self.next_table(index).is_none() {
+                assert!(!self.entries[index].flags().contains(HUGE_PAGE),
+                "Mapping huge pages is unsupported in huOS");
+                let frame = allocator.allocate_frame().expect("no frames available");
+                self.entries[index].set(frame, PRESENT | WRITABLE);
+                self.next_table_mut(index).unwrap().zero();
+            }
+            self.next_table_mut(index).unwrap()
+        }
 }
 
 pub trait TableLevel {}
@@ -74,11 +88,11 @@ pub trait HierarchicalLevel: TableLevel {
 }
 
 impl HierarchicalLevel for Level4 {
-    type NextLevel: Level3;
+    type NextLevel = Level3;
 }
 impl HierarchicalLevel for Level3 {
-    type NextLevel: Level2;
+    type NextLevel = Level2;
 }
 impl HierarchicalLevel for Level2 {
-    type NextLevel: Level1;
+    type NextLevel = Level1;
 }
