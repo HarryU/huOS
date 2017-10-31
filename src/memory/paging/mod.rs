62c7c782f421f40d7ca0cb154fc4882cf4343abc
Harry <hmucs@yahoo.co.uk>
2017-10-31 08:31:33 +0000

    Adding huge page handling, fixing page implementation and starting mapping implementation.

--- src/memory/paging/mod.rs
+++ src/memory/paging/mod.rs
@@ -17,14 +17,32 @@ pub fn translate(virtual_address: VirtualAddress) -> Option<PhysicalAddress> {
     translate_page(Page::containing_address(virtual_address)).map(|frame| frame.number * PAGE_SIZE + offset)
 }
 
-pub fn containing_address(address: VirtualAddress) -> Page {
-    assert!(address < 0x000_8000_0000_000 || address >= 0xffff_8000_000_000,
-            "invalid address: 0x{:x}", address);
-    Page { number: address / PAGE_SIZE }
-}
+impl Page {
+    pub fn containing_address(address: VirtualAddress) -> Page {
+        assert!(address < 0x000_8000_0000_000 || address >= 0xffff_8000_000_000,
+                "invalid address: 0x{:x}", address);
+        Page { number: address / PAGE_SIZE }
+    }
 
-fn start_address(&self) -> usize {
-    self.number * PAGE_SIZE
+    fn start_address(&self) -> usize {
+        self.number * PAGE_SIZE
+    }
+
+    fn p4_index(&self) -> usize {
+        (self.number >> 27) & 0o777
+    }
+
+    fn p3_index(&self) -> usize {
+        (self.number >> 18) & 0o777
+    }
+
+    fn p2_index(&self) -> usize {
+        (self.number >> 9) & 0o777
+    }
+
+    fn p1_index(&self) -> usize {
+        (self.number >> 0) & 0o777
+    }
 }
 
 fn translate_page(page: Page) -> Option<Frame> {
@@ -33,10 +51,50 @@ fn translate_page(page: Page) -> Option<Frame> {
     let p3 = unsafe { &*table::P4 }.next_table(page.p4_index());
 
     let huge_page = || {
-    }
+        p3.and_then(|p3| {
+            let p3_entry = &p3[page.p3_index()];
+            if let Some(start_frame) = p3_entry.pointed_frame() {
+                if p3_entry.flags().contains(HUGE_PAGE) {
+                    assert!(start_frame.number % (ENTRY_COUNT * ENTRY_COUNT) == 0);
+                    return Some(Frame {
+                        number: start_frame.number + page.p2_index() *
+                            ENTRY_COUNT + page.p1_index(),
+                    });
+                }
+            }
+            if let Some(p2) = p3.next_table(page.p3_index()) {
+                let p2_entry = &p2[page.p2_index()];
+                if let Some(start_frame) = p2_entry.pointed_frame() {
+                    if p2_entry.flags().contains(HUGE_PAGE) {
+                        assert!(start_frame.number % ENTRY_COUNT == 0);
+                        return Some(Frame {
+                            number: start_frame.number + page.p1_index()
+                        });
+                    }
+                }
+            }
+            None
+        })
+    };
 
     p3.and_then(|p3| p3.next_table(page.p3_index()))
-      .and_then(|p2| p2.next_table(page.p2_index()))
-      .and_then(|p1| p1.next_table(page.p1_index()))
-      .or_else(huge_page)
+        .and_then(|p2| p2.next_table(page.p2_index()))
+        .and_then(|p1| p1.next_table(page.p1_index()))
+        .or_else(huge_page)
+}
+
+pub use self::entry::*;
+use memory::FrameAllocator;
+
+pub fn map_to<A>(page: Page, frame: Frame, flags: EntryFlags,
+                 allocator: &mut A)
+    where A: FrameAllocator
+{
+    let p4 = unsafe { &mut *P4 };
+    let mut p3 = p4.next_table_create(page.p4_index(), allocator);
+    let mut p2 = p3.next_table_create(page.p3_index(), allocator);
+    let mut p1 = p2.next_table_create(page.p2_index(), allocator);
+
+    assert!(p1[page.p1_index()].is_unused());
+    p1[page.p1_index()].set(frame, flags | PRESENT);
 }
